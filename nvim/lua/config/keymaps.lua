bdbc0ae0e332b9ce2fcf3a974a052b235d1a6e9c
chrisgrounds <iamchrisgrounds@gmail.com>
2025-05-13 11:29:50 +0100

    Assign a key to group

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -3,12 +3,16 @@
 -- Add any additional keymaps here
 
 local map = vim.keymap.set
-
-map("n", "<leader>ca", function()
-  vim.lsp.buf.code_action({ apply = true })
-end, { desc = "Code action" })
-
+local wk = require("which-key")
+-- ai keys
+wk.add({
+  { "<leader>a", group = "ai" },
+})
 map("n", "<leader>ak", "<cmd>CodeCompanion<cr>", { desc = "Edit" })
 map("n", "<leader>ac", "<cmd>CodeCompanionChat<cr>", { desc = "Chat" })
 map("v", "<leader>ak", "<cmd>'<,'>CodeCompanion<cr>", { desc = "Edit" })
 map("v", "<leader>ac", "<cmd>'<,'>CodeCompanionChat<cr>", { desc = "Chat" })
+
+map("n", "<leader>ca", function()
+  vim.lsp.buf.code_action({ apply = true })
+end, { desc = "Code action" })
