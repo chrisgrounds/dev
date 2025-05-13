ceb52817f2645b08e874c6fe9778c34e7db2e707
chrisgrounds <iamchrisgrounds@gmail.com>
2025-05-13 10:30:38 +0100

    add conform

---
+++ nvim/lua/plugins/conform.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        lua = { "stylua" },
+        rust = { "rustfmt" },
+      },
+      format_on_save = {
+        -- I recommend these options. See :help conform.format for details.
+        lsp_format = "fallback",
+        timeout_ms = 500,
+      },
+    },
+  },
+}
