c8f4c5d0dc730c16ebf0da182347a7971c769f1e
chrisgrounds <iamchrisgrounds@gmail.com>
2025-05-20 21:13:20 +0100

    neo-tree

---
+++ nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      enable_diagnostics = true,
+      enable_git_status = true,
+      popup_border_style = "rounded",
+      sort_case_insensitive = false,
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = false,
+          hide_gitignored = false,
+        },
+      },
+    },
+  },
+}
