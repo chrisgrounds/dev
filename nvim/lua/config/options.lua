bbc8e76c57fe1be3f2ed2083e738d115ee368799
chrisgrounds <iamchrisgrounds@gmail.com>
2025-07-28 14:04:54 +0100

    add wrap

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -11,3 +11,4 @@ vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a T
 vim.o.softtabstop = 2 -- Number of spaces inserted instead of a TAB character
 vim.o.shiftwidth = 2
 vim.o.ignorecase = false
+vim.opt.wrap = true
