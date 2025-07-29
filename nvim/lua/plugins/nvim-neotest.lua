fbacaaccadc030fae2fcddffabe21b7894dd1e71
chrisgrounds <iamchrisgrounds@gmail.com>
2025-07-29 10:23:10 +0100

    add neotest

---
+++ nvim/lua/plugins/nvim-neotest.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "nvim-neotest/neotest",
+    optional = true,
+    dependencies = {
+      { "mrcjkb/neotest-haskell" },
+    },
+    opts = {
+      adapters = {
+        ["neotest-haskell"] = {},
+      },
+    },
+  },
+}
