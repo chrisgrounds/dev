e564c929c8b1fb27aa4403f1ea6329b32888d644
chrisgrounds <iamchrisgrounds@gmail.com>
2025-08-13 12:19:05 +0100

    add more deps

--- install.sh
+++ install.sh
@@ -7,6 +7,9 @@ dependencies=(
   "neovim"
   "zsh"
   "clang"
+  "build-essential"
+  "procps"
+  "file"
 )
 
 update_apt() {
@@ -22,6 +25,14 @@ install_oh_my_zsh() {
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 }
 
+install_brew() {
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+}
+
+install_rustup() {
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+}
+
 main() {
   echo "Updating apt"
   update_apt
@@ -29,8 +40,9 @@ main() {
   echo "Installing dependencies: ${dependencies[*]}"
   install_packages "${dependencies[@]}"
 
-  echo "Installing ohmyzsh"
   install_oh_my_zsh
+  install_rustup
+  install_brew
 
   echo "Installation complete!"
 }
