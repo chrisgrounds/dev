596de184867ece8bc807925cfe6a28957c1de0ec
chrisgrounds <iamchrisgrounds@gmail.com>
2025-08-13 12:03:13 +0100

    add missing deps and remove ai stuff

--- install.sh
+++ install.sh
@@ -5,6 +5,8 @@ dependencies=(
   "fzf"
   "bat"
   "neovim"
+  "zsh"
+  "clang"
 )
 
 update_apt() {
@@ -16,12 +18,19 @@ install_packages() {
   sudo apt update && sudo apt install -y "${packages[@]}"
 }
 
+install_oh_my_zsh() {
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+}
+
 main() {
   echo "Updating apt"
   update_apt
 
   echo "Installing dependencies: ${dependencies[*]}"
   install_packages "${dependencies[@]}"
+
+  echo "Installing ohmyzsh"
+  install_oh_my_zsh
 
   echo "Installation complete!"
 }
--- sync.sh
+++ sync.sh
@@ -14,7 +14,6 @@ fi
 ### variables ###
 linux_home="/home/$LINUX_USERNAME"
 windows_home="/mnt/c/Users/$WINDOWS_USERNAME"
-oai_api_key=$(cat "$linux_home/.config/openai/key")
 
 ### functions ###
 remove_dir() {
@@ -32,19 +31,20 @@ copy() {
   cp -r $1 $2
 }
 
+### config ###
+config_path="$linux_home/.config"
+remove_dir "$config_path"
+copy nvim "$config_path"
+
 ### nvim ###
 nvim_path="$linux_home/.config/nvim"
 remove_dir "$nvim_path"
 copy nvim "$nvim_path"
-sed -i -e "s/OAI_API_KEY/$oai_api_key/g" "$nvim_path/lua/plugins/codecompanion.lua"
 
 ### wezterm ###
 w11_wezterm_path="$windows_home/.wezterm.lua"
 remove_file "$w11_wezterm_path"
 copy wezterm/wezterm.lua "$w11_wezterm_path"
-
-### claude ###
-copy ai/claude/CLAUDE.md "$linux_home/.claude/CLAUDE.md"
 
 ### zsh ###
 copy zsh/zshrc "$linux_home/.zshrc"
