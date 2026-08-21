b92ce72430b86fa28cdc7fbd6bb194ade3b7839c
chrisgrounds <iamchrisgrounds@gmail.com>
2026-08-21 09:49:59 +0100

    add ghostty and remove wsl

--- sync.sh
+++ sync.sh
@@ -1,55 +1,41 @@
 #!/bin/bash
 
-### envvar check ###
-if [[ -z "${LINUX_USERNAME}" ]]; then
-  echo "Missing LINUX_USERNAME envvar"
-  exit 1
-fi
-
-if [[ -z "${WINDOWS_USERNAME}" ]]; then
-  echo "Missing WINDOWS_USERNAME envvar"
-  exit 1
-fi
-
-### variables ###
-linux_home="/home/$LINUX_USERNAME"
-windows_home="/mnt/c/Users/$WINDOWS_USERNAME"
-
 ### functions ###
 remove_dir() {
   echo "Removing $1"
-  rm -rf $1
+  rm -rf "$1"
 }
 
 remove_file() {
   echo "Removing $1"
-  rm $1
+  rm -f "$1"
 }
 
 copy() {
   echo "Copying $1 => $2"
-  cp -r $1 $2
+  cp -r "$1" "$2"
 }
 
-### config ###
-config_path="$linux_home/.config"
-remove_dir "$config_path"
-copy nvim "$config_path"
-
 ### nvim ###
-nvim_path="$linux_home/.config/nvim"
+nvim_path="$HOME/.config/nvim"
 remove_dir "$nvim_path"
 copy nvim "$nvim_path"
 
-### wezterm ###
-w11_wezterm_path="$windows_home/.wezterm.lua"
-remove_file "$w11_wezterm_path"
-copy wezterm/wezterm.lua "$w11_wezterm_path"
-
 ### zsh ###
-copy zsh/zshrc "$linux_home/.zshrc"
+copy zsh/zshrc "$HOME/.zshrc"
 
 ### herdr ###
-herdr_path="$linux_home/.config/herdr"
+herdr_path="$HOME/.config/herdr"
 remove_dir "$herdr_path"
 copy herdr "$herdr_path"
+
+### wezterm ###
+wezterm_path="$HOME/.wezterm.lua"
+remove_file "$wezterm_path"
+copy wezterm/wezterm.lua "$wezterm_path"
+
+### ghostty ###
+ghostty_path="$HOME/Library/Application Support/com.mitchellh.ghostty/config"
+remove_file "$ghostty_path"
+mkdir -p "$(dirname "$ghostty_path")"
+copy ghostty/config "$ghostty_path"
